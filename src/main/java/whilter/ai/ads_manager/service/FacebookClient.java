d5feea914fca84971ff3b31d44d9bfec76ac4da5
dharmendra <dhirubhaigpl@gmail.com>
2025-03-23 07:49:56 +0530

    posting video on instagram working

--- src/main/java/whilter/ai/ads_manager/service/FacebookClient.java
+++ src/main/java/whilter/ai/ads_manager/service/FacebookClient.java
@@ -1,11 +1,14 @@
 package whilter.ai.ads_manager.service;
 
 import feign.Headers;
+import feign.Param;
 import feign.RequestLine;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Map;
@@ -13,11 +16,13 @@ import java.util.Map;
 @FeignClient(name = "facebookClient", url = "https://graph.facebook.com/v22.0")
 public interface FacebookClient {
 
+//    @PostMapping("/{igUserId}/media")
+//    Map<String, Object>  createPost(@PathVariable("igUserId") String igUserId,
+//                      @RequestParam("caption") String caption,
+//                      @RequestParam("image_url") String imageUrl,
+//                      @RequestParam("access_token") String accessToken);
     @PostMapping("/{igUserId}/media")
-    Map<String, Object>  createPost(@PathVariable("igUserId") String igUserId,
-                      @RequestParam("caption") String caption,
-                      @RequestParam("image_url") String imageUrl,
-                      @RequestParam("access_token") String accessToken);
+    Map<String, Object> createPost(@PathVariable("igUserId") String igUserId, @RequestBody Map<String, Object> requestBody);
 
 //    @PostMapping("/{igUserId}/media")
 //    String createVideoPost(@PathVariable("igUserId") String igUserId,
@@ -30,16 +35,17 @@ public interface FacebookClient {
 //                        @RequestParam("creation_id") String creationId,
 //                        @RequestParam("access_token") String accessToken);
 
-    @RequestLine("POST /{igUserId}/media")
-    @Headers("Content-Type: application/json")
-    Map<String, Object> createVideoPost(@PathVariable("igUserId") String igUserId,
-                                        @RequestBody Map<String, Object> requestBody);
+    @PostMapping("/{igUserId}/media")
+    Map<String, Object> createVideoPost(@PathVariable("igUserId") String igUserId, @RequestBody Map<String, Object> requestBody);
 
-    @RequestLine("POST /{igUserId}/media_publish")
-    @Headers("Content-Type: application/json")
-    Map<String, Object> publishMedia(@PathVariable("igUserId") String igUserId,
-                                         @RequestBody Map<String, Object> requestBody);
+    @PostMapping("/{igUserId}/media_publish")
+    Map<String, Object> publishMedia(@PathVariable("igUserId") String igUserId, @RequestBody Map<String, Object> requestBody);
 
+//    @GetMapping("/{media_id}?fields=status_code")
+//    Map<String, Object> getMediaStatus(@PathVariable("media_id") String mediaId, @RequestParam String accessToken);
 
+    @GetMapping("/{mediaId}?fields=status_code")
+    Map<String, Object> getMediaStatus(@PathVariable("mediaId") String mediaId,
+                                       @RequestHeader("Authorization") String accessToken);
 }
 
--- src/main/java/whilter/ai/ads_manager/service/PostHandler.java
+++ src/main/java/whilter/ai/ads_manager/service/PostHandler.java
@@ -1,11 +1,11 @@
 package whilter.ai.ads_manager.service;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.stereotype.Component;
 import whilter.ai.ads_manager.model.PostRequest;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 
 @Component
@@ -24,14 +24,7 @@ public class PostHandler implements InstagramHandler {
 
         if (request.getImageUrl()==null || request.getImageUrl().isEmpty()) {
             try {
-                String videoUrl = request.getVideoUrl();
-//                String response = facebookClient.createVideoPost(instaBusinessIdString, videoUrl, caption, accessToken);
-
-                Map<String, Object> videoPostRequest = new HashMap<>();
-                videoPostRequest.put("media_type", "VIDEO");  // Important: Explicitly set media_type
-                videoPostRequest.put("video_url", videoUrl);
-                videoPostRequest.put("caption", caption);
-                videoPostRequest.put("access_token", accessToken);
+                var videoPostRequest = getStringObjectMap(request, caption, accessToken);
 
                 Map<String, Object> response = facebookClient.createVideoPost(instaBusinessIdString, videoPostRequest);
                 if (!response.containsKey("id")) {
@@ -45,14 +38,30 @@ public class PostHandler implements InstagramHandler {
             }
         } else {
             try {
-                String imageUrl = request.getImageUrl();
-                Map<String, Object> response = facebookClient.createPost(instaBusinessIdString, caption, imageUrl, accessToken);
+                Map<String, Object> imagePostRequest = new HashMap<>();
+                imagePostRequest.put("image_url", request.getImageUrl());
+                imagePostRequest.put("caption", caption);
+                imagePostRequest.put("access_token", accessToken);
+
+                Map<String, Object> response = facebookClient.createPost(instaBusinessIdString, imagePostRequest);
                 Map<String, Object> publishResponse = publishMedia(response, accessToken);
 
             } catch (Exception e) {
                 System.err.println("Video Post failed: " + e.getMessage());
             }
         }
+    }
+
+    private static Map<String, Object> getStringObjectMap(PostRequest request, String caption, String accessToken) {
+        String videoUrl = request.getVideoUrl();
+//                String response = facebookClient.createVideoPost(instaBusinessIdString, videoUrl, caption, accessToken);
+
+        Map<String, Object> videoPostRequest = new HashMap<>();
+        videoPostRequest.put("media_type", "REELS");  // Important: Explicitly set media_type
+        videoPostRequest.put("video_url", videoUrl);
+        videoPostRequest.put("caption", caption);
+        videoPostRequest.put("access_token", accessToken);
+        return videoPostRequest;
     }
 
     private Map<String, Object> publishMedia(Map<String, Object> response, String accessToken){
@@ -63,6 +72,21 @@ public class PostHandler implements InstagramHandler {
 //            Map jsonMap = objectMapper.readValue(response, Map.class);
             String creationId = (String) response.get("id");
             System.out.println("Container created successfully. ContainerID: " + creationId);
+            String status;
+            do {
+                // ✅ Check the media status
+                Map<String, Object> statusResponse = facebookClient.getMediaStatus(creationId,  "Bearer " + accessToken);
+                status = (String) statusResponse.get("status_code");
+
+                if ("FINISHED".equals(status)) {
+                    System.out.println("Media status: " + status);
+                    break;
+                }
+
+                // ✅ Wait 5 seconds before checking again
+                TimeUnit.SECONDS.sleep(5);
+                System.out.println("Media status: " + status);
+            } while ("IN_PROGRESS".equals(status));
 
             Map<String, Object> publishRequest = new HashMap<>();
             publishRequest.put("creation_id", creationId);
